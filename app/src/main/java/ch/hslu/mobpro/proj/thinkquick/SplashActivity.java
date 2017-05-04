4850e2c9c300c754d371259e7b1adfec95a746ca
alkazua <alan.meile@stud.hslu.ch>
2017-05-04 16:25:37 +0200

    Created SplashScreen Activity for the start of the app

---
+++ app/src/main/java/ch/hslu/mobpro/proj/thinkquick/SplashActivity.java
@@ -0,0 +1,31 @@
+package ch.hslu.mobpro.proj.thinkquick;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int MAINACTIVITY_DELAY = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        startMainActivityDelayed(this);
+    }
+
+    private void startMainActivityDelayed(final Context context) {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, MAINACTIVITY_DELAY);
+    }
+}

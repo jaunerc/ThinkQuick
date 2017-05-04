bb0d6416276616ffb7f594f61706fa248206caac
alkazua <alan.meile@stud.hslu.ch>
2017-05-04 21:02:56 +0200

    Created the views Menu, Game and Highscore

--- app/src/main/java/ch/hslu/mobpro/proj/thinkquick/MainActivity.java
+++ app/src/main/java/ch/hslu/mobpro/proj/thinkquick/MainActivity.java
@@ -1,7 +1,10 @@
 package ch.hslu.mobpro.proj.thinkquick;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Intent gameActivity = new Intent(this, GameActivity.class);
+        final Intent hightscoreActivity = new Intent(this, HighscoreActivity.class);
+
+        Button play = (Button) findViewById(R.id.mainPlay);
+        Button highscore = (Button) findViewById(R.id.mainHighscore);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(gameActivity);
+                finish();
+            }
+        });
+
+        highscore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(hightscoreActivity);
+                finish();
+            }
+        });
     }
 }
